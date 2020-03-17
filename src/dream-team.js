c810e47623ac73778ba181fd01fd51962c3779b2
Priuet <nancy56@list.ru>
2020-03-17 21:51:15 +0800

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if ((toString.call(members).slice(8, -1) !== 'Array') || members.length === 0) {
+        return false;
+    } else {
+        members = members.filter((elem) => typeof(elem) === 'string');
+        if (members.length === 0) return false;
+        members = members.map((elem) => elem.trim().toUpperCase()
+                .split('')
+                .slice(0, 1)
+                .join(''))
+            .sort()
+            .join('');
+        return members;
+    }
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,28 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!arguments.length) {
+        return 'Unable to determine the time of year!';
+    } else {
+        if (toString.call(date).slice(8, -1) !== 'Date') {
+            throw new Error();
+        } else {
+            let month = date.getMonth();
+            let season = '';
+            if (month === 0 || month === 1 || month === 11) {
+                season = 'winter';
+            } else {
+                if (month >= 2 && month < 5) {
+                    season = 'spring';
+                } else {
+                    if (month >= 5 && month < 8) {
+                        season = 'summer';
+                    } else {
+                        if (month >= 8 && month < 11) {
+                            season = 'autumn';
+                        }
+                    }
+                }
+            }
+            return season;
+        }
+    }
 };
